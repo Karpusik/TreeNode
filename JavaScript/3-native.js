5db44e8ef78278abeeb78fdbb3da126b262011c1
Karpusik <karpova0609@gmail.com>
2016-12-26 18:36:40 +0200

    Update 3-native.js

--- JavaScript/3-native.js
+++ JavaScript/3-native.js
@@ -14,3 +14,26 @@ let n2 = new Node(root, 'n2' );
 let n3 = new Node(root, 'n3' );
 
 console.dir(root, { depth: null });
+
+function isEqual(obj1,obj2){ 
+
+	for(let key in obj1){ 
+		if(obj1[key]!=obj2[key])return false; 
+
+	} 
+	return true; 
+		} 
+		
+//рекурсивно проходим по дереву, если находим равный объект возвращаем его 
+function search(tree, query){ 
+
+	for(let node in tree){ 
+		if( typeof tree[node] === 'object'){ 
+
+			if(isEqual(tree[node],query))return tree[node]; 
+
+			var res = search(tree[node],query); 
+		if(res)return res; 
+		} 
+	} 
+}
